ecfd32f00343cd27c82aa98facad200d936236b1
Dean Scelza and Tony Fuentes <dean@>
2016-08-17 15:15:53 -0700

    complete JS logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
-
-
-
   $("#sentence_field").submit(function(event){
     event.preventDefault();
 
     var inputSentence = $("input#sentenceinput").val();
     var words = inputSentence.split(" ");
     var pigLatinWords = [];
-    var vowels = ["a", "e", "i", "o", "u"];
+    var vowels = ["a", "e", "i", "o", "u","y"];
     var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
 
     var isVowell = function(letter) {
@@ -30,84 +27,59 @@ $(document).ready(function(){
     };
 
     for (var i = 0; i < words.length; i++){
-
       var wordLetters = words[i].toLowerCase().split("");
+      var firstVowel = -1;
+      var quExists = false;
+      var firstQ = -1;
 
-      console.log(wordLetters);
-
-      /*if(wordLetters.length === 1 && isVowell(wordLetters[0])) {
-        console.log("yes");
-        pigLatinWords.push(wordLetters[0] + "ay");
+      for(var j=0; j < wordLetters.length; j++){
+        if(j === 0 && wordLetters[j] === "y") {}
+        else if(isVowell(wordLetters[j])) {
+          firstVowel = j;
+          break;
+        }
       }
 
-      else if(wordLetters.length > 1 && isVowell(wordLetters[0])) {
-        console.log("yes");
-        pigLatinWords.push(words[i].toLowerCase() + "ay");
-      }*/
+      firstQ = wordLetters.indexOf("q");
+      if(firstQ > -1 && firstQ < firstVowel && wordLetters[firstQ+1]=== "u" )
+        quExists = true;
 
-      if(isVowell(wordLetters[0])) {
-        pigLatinWords.push(words[i].toLowerCase() + "ay");
+      console.log(wordLetters + ", FV:" + firstVowel + ", FQ:" + firstQ);
+
+      if(firstVowel === 0 && wordLetters[0] != "y") {
+        console.log("Case1");
+        pigLatinWords.push(words[i] + "ay");
       }
-
-      else if(isConsonant(wordLetters[0])) {
-
-        var temp = wordLetters[0];
-        wordLetters.shift();
+      else if (quExists) {
+        console.log("Case2");
+        for(var j=0; j <= (firstQ +1); j++){
+          wordLetters.push(wordLetters.shift());
+        }
+        pigLatinWords.push(wordLetters.join("") + "ay");
+      }
+      else if(firstVowel === 1 && (isConsonant(wordLetters[0]) || wordLetters[0] === "y") ){
+        console.log("Case4");
+        var temp = wordLetters.shift();
         wordLetters.push(temp);
-        pigLatinWords.push(wordLetters.join("").toLowerCase() + "ay");
+        pigLatinWords.push(wordLetters.join("") + "ay");
       }
-
+      else if(firstVowel >= 2) {
+        console.log("Case5");
+        for(var j=0; j<firstVowel; j++){
+          wordLetters.push(wordLetters.shift());
+        }
+        pigLatinWords.push(wordLetters.join("") + "ay");
+      }
       else {
-        pigLatinWords.push(wordLetters[0]);
+        console.log("Case6");
+        pigLatinWords.push(wordLetters.join(""));
       }
-
     }
 
     console.log("Pig Latin: " + pigLatinWords);
 
-
+    $(".pigAnswer").detach();
+    $("#sentenceOutput").append("<h3 class=\"pigAnswer\">" + pigLatinWords.join(" ") + "</h3>")
 
   });
 });
-
-
-
-/*
-
-
-
-
-
-For words beginning with one consonant, move the first consonant to the end, and add "ay".
-	◦	Example Input: dog
-	◦	Example Output: ogday
-For words beginning multiple consonants, move all of the first consecutive consonants to the end, and add "ay".
-	◦	Example Input: string
-	◦	Example Output: ingstray
-If the first consonants include "qu", move the "u" along with the "q".
-	◦	Example Input: queen
-	◦	Example Output: eenquay
-If the first consonants are not “qu” but there is a “qu” before the first vowell, move the consonants and ”u” along with the "q".
-	◦	Example Input: squeal
-	◦	Example Output: ealsquay
-For words beginning with "y", treat "y" as a consonant.
-	◦	Example Input: year
-	◦	Example Output: earyay
-
-
-
-
-    The program adds "ay" to single-letter words beginning with a vowel.
-    	◦	Example Input: i
-    	◦	Example Output: iay
-
-      The program adds "ay" to multi-letter words beginning with a vowel.
-      	◦	Example Input: in
-      	◦	Example Output: inay
-
-      The program does nothing to non-alphabetical characters, since they do not contain consonants or vowels.
-        	◦	Example Input: 3
-        	◦	Example Output: 3
-
-
-*/
